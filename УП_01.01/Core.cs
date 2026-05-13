a49433ae45cb98e51d3719b4d588cd4b9293bbc5
234958 <234958@KB37-118-C11>
2026-05-13 11:17:52 +0300

    Подключение бд к проекту

---
+++ УП_01.01/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УП_01._01
+{
+    internal class Core
+    {
+        public static ReadWriteEntities Context = new ReadWriteEntities();
+    }
+}
