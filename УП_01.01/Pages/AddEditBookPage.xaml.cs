e978a4ea1cb86a3bad4af1746f06e43f4b5a4224
234958 <234958@KB37-118-C11>
2026-05-14 09:42:32 +0300

    Добавлена страница просмотра своих книг для авторов

---
+++ УП_01.01/Pages/AddEditBookPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace УП_01._01.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditBookPage.xaml
+    /// </summary>
+    public partial class AddEditBookPage : Page
+    {
+        private int? _bookId;
+
+        public AddEditBookPage(int? bookId)
+        {
+            InitializeComponent();
+            _bookId = bookId;
+        }
+    }
+}
